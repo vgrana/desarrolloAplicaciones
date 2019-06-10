b02f331536f718d2f2a125acf73d15818ccb71a9
valeria <valeriagrandi@hotmail.com>
2019-06-10 20:53:08 -0300

    cambio de color de fondo,agregado del link de materializable, framework de css para estilos, y del scrip correspondiente

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 server = require("./server")
+Producto = require("./src/producto")
+Cliente = require("./src/cliente")
 
 mongoConnection = require("./src/mongo/mongoConnection")
 Home = require("./src/mongo/mongoHome")
 mongoConnection.connect( (db) => {
     productoHome = new Home("productos", db)
     clienteHome = new Home("clientes", db)    
+   
+    productoHome.insert(new Producto("alfajor", 20))
+    productoHome.insert(new Producto("chocolate", 30))
+
+    clienteHome.insert(new Cliente("pepito", "calle falsa 123"))
+    clienteHome.insert(new Cliente("fulanito", "calle san martín s/n"))
+   
     server.register(productoHome)
     server.register(clienteHome)
     server.init();
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Productos from "./componentes/Productos"
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

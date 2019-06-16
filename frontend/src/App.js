eb672ba3dedb948e1780dea1cf9abc0722674920
valeria <valeriagrandi@hotmail.com>
2019-06-16 20:25:29 -0300

    barra con botones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import EntityList from './components/EntityList'
-import './App.css';
 
+import EntityList from './components/EntityList'
+import HomeComponent from './components/HomeComponent'
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom"
+import './App.css';
+//NavLink
+
+function ClientesComponent(){
+  return (<EntityList entity="clientes"/>)
+}
+function ProductosComponent(){
+  return (<EntityList entity="productos"/>)
+}
 function App() {
   return (
     <div className="App">
+    <Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <EntityList entity="clientes" />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={logo} className="App-logo" alt="logo" />*/}
+            
+{/*import logo from './logo.svg';*/}
+        {/* <ul>  
+         <li><NavLink to ="/">Home </NavLink></li>
+         <li><NavLink to ="/clientes">Clientes </NavLink></li>
+         <li><NavLink to ="/productos">Productos</NavLink></li>
+       </ul> */}
+       <nav>
+         <div className="nav-wrapper">
+         <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a className="waves-effect waves-light btn" href= "/">Home</a></li>
+             <li><a className="waves-effect waves-light btn" href= "/clientes">Clientes</a></li>
+             <li><a className="waves-effect waves-light btn" href= "/productos">Productos</a></li>
+        </ul>
+      
+       </div>
+       </nav>
+
+      </header> 
+      <main className="App-main">
+        <Switch>
+            <Route path="/" exact component={HomeComponent} />
+            <Route path="/clientes"  component={ClientesComponent} />
+            <Route path="/productos" component={ProductosComponent} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/HomeComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HomeComponent extends React.Component {
+
+    render() {
+      return (
+        <div>
+          <h1>
+            Aplicación para manejar clientes y productos
+          </h1>
+        </div>
+      );
+    }
+  }
+
+
+
+  export default HomeComponent
